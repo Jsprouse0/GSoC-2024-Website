99b927a08f7ab469d578375e4baa55da9128ef2c
Jacob Sprouse <95829867+Jsprouse0@users.noreply.github.com>
2024-08-30 12:59:38 -0500

    Updated Website
    finished Javascript files to toggle color themes and dark/light mode

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* typing animation */
+var typed = new Typed(".typing", {
+    strings: ["", "A.I. Engineer", "NetPyNE Developer", "Google Summer of Code 2024 Contributor"],
+    typeSpeed: 100,
+    BackSpeed: 90,
+    loop: true
+});
